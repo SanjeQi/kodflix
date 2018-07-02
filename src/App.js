dbf2fe666b2318f9c48eaa6e1e18b0b1533625e6
Cristian Constantinescu <cristi.nord@gmail.com>
2018-07-02 11:26:11 +0100

    done #1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import picture from './2.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
           <h1 className="App-title">Welcome to kodeflix</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <img src={picture} className="" alt="logo" />
         </p>
       </div>
     );
