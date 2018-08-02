918335b7ae26b5cf39884f9566f04ad509c75967
Cristian Constantinescu <cristi.nord@gmail.com>
2018-08-02 14:26:26 +0100

    #18 Introduce a back-end server #18

--- src/frontend/Cover/Cover.js
+++ src/frontend/Cover/Cover.js
@@ -4,7 +4,7 @@ import "./Cover.css";
 
 export default function Cover(props) {
     return (
-        <div className="item">
+        <div className="cover">
             <Link to={props.id} className='item'>
                 <img src={props.img} className="image" alt={props.title} />
                 <div className="movieTitle"><h1>{props.title}</h1></div>
