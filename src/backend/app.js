dfb4d7ae029a770ebceb9daec3b28a24aa76ece8
Cristian Constantinescu <cristi.nord@gmail.com>
2018-08-03 10:01:32 +0100

    #19 Return a list of shows from the backend #19

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,6 +1,9 @@
-const express = require('express' )
-const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const express = require('express');
+const app = express();
+const getShows = require('./shows');
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.get('/rest/shows', (req, res) => res.send(getShows()));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));   
---
+++ src/backend/shows.js
@@ -0,0 +1,53 @@
+
+
+module.exports = function getShows() {
+    return [
+        {
+            title: 'Black Mirror',
+            id: 'black_mirror',
+            synopsis: `
+                An anthology series exploring a twisted, high-tech world where
+                humanity's greatest innovations and darkest instincts collide.`,
+        },
+        {
+            title: 'Breaking Bad',
+            id: 'breaking_bad',
+            synopsis: `
+                A high school chemistry teacher diagnosed with inoperable lung 
+                cancer turns to manufacturing and selling methamphetamine in order
+                to secure his family's future.`,
+        },
+        {
+            title: 'Death Note',
+            id: 'death_note',
+            synopsis: `
+                An intelligent high school student goes on a secret crusade to
+                eliminate criminals from the world after discovering a notebook
+                capable of killing anyone whose name is written into it.`,
+        },
+        {
+            title: 'Games of Thrones',
+            id: 'games_of_thrones',
+            synopsis: `
+                Nine noble families fight for control over the mythical lands 
+                of Westeros, while an ancient enemy returns after being dormant 
+                for thousands of years.`,
+        },
+        {
+            title: 'The Walking Dead',
+            id: 'the_walking_dead',
+            synopsis: `
+                Sheriff Deputy Rick Grimes wakes up from a coma to learn the 
+                world is in ruins, and must lead a group of survivors to stay 
+                alive`,
+        },
+        {
+            title: 'The Wire',
+            id: 'the_wire',
+            synopsis: `
+                Baltimore drug scene, seen through the eyes of drug dealers and
+                law enforcement.`,
+        }
+    ];
+};
+
